19d88e9b7fe0208159630c55abbb109af8acdee5
damixiorek <damixiorek@gmail.com>
2020-11-28 17:19:48 +0100

    mod main.cpp, add break duration

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,49 @@
 #include <BinaryClock.hpp>
 #include <BinaryFromDecimalConverter.hpp>
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
-#include <fcntl.h>
+
+#include <chrono>
+#include <thread>
 
 #include <iostream>
- 
+
 int kbhit(void)
 {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
-}
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
 
 int main()
 {
     BinaryClock bc;
 
     while (!kbhit()) {
+        std::chrono::nanoseconds breaker(1000);
+        std::this_thread::sleep_for(breaker);
         bc.createTime();
         system("clear");
     }
