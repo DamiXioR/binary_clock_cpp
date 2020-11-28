7e1a0449a8257b7b53d0f0c6cbfd235397ceab32
damixiorek <damixiorek@gmail.com>
2020-11-28 17:34:11 +0100

    mod ClockScreen class - showScreen correct order

--- libs/ClockScreen.cpp
+++ libs/ClockScreen.cpp
@@ -1,29 +1,33 @@
 #include <ClockScreen.hpp>
 
+#include <algorithm>
 #include <iostream>
 
 void ClockScreen::showScreen(TimeParser* timeParser, BinaryTimeUnitsHolder* binaryTimeUnitsHolder) const
 {
-
     std::cout << "BINARY CLOCK\n";
     std::cout << "PRESS ANY KEY FOR BREAK\n";
     std::cout << "*******************************\n";
     std::cout << "*******************************\n";
 
-    for (const auto& h : binaryTimeUnitsHolder->binaryHour_) {
-        std::cout << h << " ";
-    }
+    std::for_each(binaryTimeUnitsHolder->binaryHour_.crbegin(), binaryTimeUnitsHolder->binaryHour_.crend(),
+                  [](bool everyBit) {
+                      std::cout << everyBit << " ";
+                  });
     std::cout << "\n";
 
-    for (const auto& m : binaryTimeUnitsHolder->binaryMinutes_) {
-        std::cout << m << " ";
-    }
+    std::for_each(binaryTimeUnitsHolder->binaryMinutes_.crbegin(), binaryTimeUnitsHolder->binaryMinutes_.crend(),
+                  [](bool everyBit) {
+                      std::cout << everyBit << " ";
+                  });
     std::cout << "\n";
 
-    for (const auto& s : binaryTimeUnitsHolder->binarySeconds_) {
-        std::cout << s << " ";
-    }
+    std::for_each(binaryTimeUnitsHolder->binarySeconds_.crbegin(), binaryTimeUnitsHolder->binarySeconds_.crend(),
+                  [](bool everyBit) {
+                      std::cout << everyBit << " ";
+                  });
     std::cout << "\n";
+    
     std::cout << "*******************************\n";
     std::cout << "*******************************\n";
 }
